353db2e7f0730a082361f2f02299fc143357555d
Hatanaka, Akihiro <hatanaka.akihiro@gmail.com>
2016-07-27 00:53:08 +0000

    display average rewards and steps change training iterations

--- taxi/q/main.py
+++ taxi/q/main.py
@@ -7,6 +7,8 @@ print(env.observation_space)
 
 agent = QAgent(env.observation_space, env.action_space);
 agent.learn(env)
+total_reward = 0
+total_step = 0
 for i_episode in range(100):
 	episode_reward = 0
 	episode_step = 0
@@ -19,4 +21,8 @@ for i_episode in range(100):
 		episode_step += 1
                 if done:
                         print("Episode {} has done. reward is {}. step is {}.".format(i_episode, episode_reward, episode_step))
+			total_reward += episode_reward
+			total_step += episode_step
 			break
+print("Average Reward is {}. Step is {}."
+	.format(total_reward / 100, total_step / 100))
